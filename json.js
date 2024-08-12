6c22546f4352cd6ec22d0acbfa21298219c10e28
Jose Rosal <andresrosalaponteee@gmail.com>
2024-08-12 13:05:25 -0400

    funciona el crear html en el primer tab

---
+++ json.js
@@ -0,0 +1,113 @@
+const hamburguers=[
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger.png",
+        price:15.00
+    }
+];
+
+const tacos=[
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger1.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger1.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger1.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger1.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger1.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger1.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger1.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger1.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger1.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hambuerger1.png",
+        price:15.00
+    }
+]
--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-document.addEventListener("DOMContentLoaded",createHtml)
+document.addEventListener("DOMContentLoaded",()=>{
+  console.log("holas dom")
+  createHtml(hamburguers,".box-container-1")
+})
+// console.log(hamburguers,"obj")
 
 function E(selector,parent){
     // console.log(selector,"hola selector");
@@ -19,7 +23,7 @@ function hasClass(element,clasName) {
 function radioClass(element,clasName){
     // console.log(clasName,"hola clasName")
     // console.log(element,"hola element")
-    console.log(E("." + clasName));
+    // console.log(E("." + clasName));
     E("." + clasName).forEach(elem => elem.classList.remove(clasName));
     element.classList.toggle(clasName);
 }
@@ -49,106 +53,123 @@ function tabs(nav) {
     }
 }
 
-function createHtml(){
-    container =document.querySelector(".box-container-1")
-    for (let index = 0; index < 4; index++) {
+
+
+function createHtml(obj,selector){
+    console.log(obj,"hola objeto")
+    container =document.querySelector(selector)
+    obj.forEach(box=>{
+      const {name,description,img,price}=box;
       let html=`
-        <div class="box-1">
-          <div class="image">
-              <img src="/images/hambuerger.png" alt="">
-          </div>
-
-          <div class="content">
-
-              <h3>Burger Premium</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!</p>
-              <div class="icons">
-                  <span class="price">$15.00</span>
-                  <span class="buy">
-                      <img src="/images/car.svg" alt="">
-                  </span>
-              </div>
-
-          </div>
+      <div class="box-1">
+        <div class="image">
+            <img src="${img}" alt="">
         </div>
-      `;
-  
-      container.innerHTML+= html
+
+        <div class="content">
+
+            <h3>${name}</h3>
+            <p>${description}</p>
+            <div class="icons">
+                <span class="price">$${price}</span>
+                <span class="buy">
+                    <img src="/images/car.svg" alt="">
+                </span>
+            </div>
+
+        </div>
+      </div>
+    `;
+
+    container.innerHTML+= html
+    })
+
       
-    }
+    
 }
 
 tabs(".menu-nav");
 
-let loadMoreBtn1 = document.querySelector('#load-more-1');
-let currentItem1 = 4;  // Starting index for displaying items
+// let loadMoreBtn1 = document.querySelector('#load-more-1');
+// let currentItem1 = 4;  // Starting index for displaying items
 
-loadMoreBtn1.onclick = () => {
-  createHtml()
-  // console.log("ingreso a load more"); // Optional console log for debugging
+// loadMoreBtn1.onclick = () => {
+//   createHtml()
+//   // console.log("ingreso a load more"); // Optional console log for debugging
 
-  // Get all box elements within the container with class "box-container-1"
-    let boxes = [...document.querySelectorAll(".box-container-1 .box-1")];
-    console.log(boxes,"hola boxes")
-  // Loop to display the next 4 items
-    for (let i = currentItem1; i < currentItem1 + 4 && i < boxes.length; i++) {
-        console.log("dentro del for");
-        boxes[i].style.display = 'inline-block';
-    }
+//   // Get all box elements within the container with class "box-container-1"
+//     let boxes = [...document.querySelectorAll(".box-container-1 .box-1")];
+//     console.log(boxes,"hola boxes")
+//   // Loop to display the next 4 items
+//     for (let i = currentItem1; i < currentItem1 + 4 && i < boxes.length; i++) {
+//         console.log("dentro del for");
+//         boxes[i].style.display = 'inline-block';
+//     }
 
-  // Update currentItem for the next click
-    currentItem1 += 4;
+//   // Update currentItem for the next click
+//     currentItem1 += 4;
 
-    // Hide the button if all items have been displayed
-    if (currentItem1 >= boxes.length) {
-        loadMoreBtn1.style.display = 'none';
-    }
-};
+//     // Hide the button if all items have been displayed
+//     if (currentItem1 >= boxes.length) {
+//         loadMoreBtn1.style.display = 'none';
+//     }
+// };
 
-let loadMoreBtn2 = document.querySelector('#load-more-2');
-let currentItem2 = 4;  // Starting index for displaying items
+let loadMoreBtn1 = document.querySelector('.load-more-1');
+let loadMoreBtn2 = document.querySelector('.load-more-2');
+let loadMoreBtn3 = document.querySelector('.load-more-3');
+ // Starting index for displaying items
 
-loadMoreBtn2.onclick = () => {
-  // console.log("ingreso a load more"); // Optional console log for debugging
+loadMoreBtn1.addEventListener("click",load)
+loadMoreBtn2.addEventListener("click",load)
+loadMoreBtn3.addEventListener("click",load)
+var currentItem = 4; 
 
+
+function load(e){
+  
+  console.log("ingreso a load more"); // Optional console log for debugging
+  let id= parseInt(e.target.id);
+  console.log(e.target,"target")
   // Get all box elements within the container with class "box-container-2"
-    let boxes = [...document.querySelectorAll(".box-container-2 .box-2")];
-
+    let boxes = [...document.querySelectorAll(`.box-${id}`)];
+  console.log(boxes)
   // Loop to display the next 4 items
-    for (let i = currentItem2; i < currentItem2 + 4 && i < boxes.length; i++) {
-        console.log("dentro del for");
+    for (let i = currentItem; i < currentItem + 4 && i < boxes.length; i++) {
+        console.log("dentro del for",i);
+        
+        console.log(currentItem)
         boxes[i].style.display = 'inline-block';
     }
 
   // Update currentItem for the next click
-    currentItem2 += 4;
-
+    currentItem+=4
     // Hide the button if all items have been displayed
-    if (currentItem2 >= boxes.length) {
-        loadMoreBtn2.style.display = 'none';
+    if (currentItem >= boxes.length) {
+        e.target.style.display = 'none';
     }
 };
 
-let loadMoreBtn3 = document.querySelector('#load-more-3');
-let currentItem3 = 4;  // Starting index for displaying items
+// let loadMoreBtn3 = document.querySelector('#load-more-3');
+// let currentItem3 = 4;  // Starting index for displaying items
 
-loadMoreBtn3.onclick = () => {
-  // console.log("ingreso a load more"); // Optional console log for debugging
+// loadMoreBtn3.onclick = () => {
+//   // console.log("ingreso a load more"); // Optional console log for debugging
 
-  // Get all box elements within the container with class "box-container-3"
-    let boxes = [...document.querySelectorAll(".box-container-3 .box-3")];
+//   // Get all box elements within the container with class "box-container-3"
+//     let boxes = [...document.querySelectorAll(".box-container-3 .box-3")];
 
-  // Loop to display the next 4 items
-    for (let i = currentItem3; i < currentItem3 + 4 && i < boxes.length; i++) {
-        console.log("dentro del for");
-        boxes[i].style.display = 'inline-block';
-    }
+//   // Loop to display the next 4 items
+//     for (let i = currentItem3; i < currentItem3 + 4 && i < boxes.length; i++) {
+//         console.log("dentro del for");
+//         boxes[i].style.display = 'inline-block';
+//     }
 
-  // Update currentItem for the next click
-    currentItem3 += 4;
+//   // Update currentItem for the next click
+//     currentItem3 += 4;
 
-    // Hide the button if all items have been displayed
-    if (currentItem3 >= boxes.length) {
-        loadMoreBtn3.style.display = 'none'; 
-    }
-};
+//     // Hide the button if all items have been displayed
+//     if (currentItem3 >= boxes.length) {
+//         loadMoreBtn3.style.display = 'none'; 
+//     }
+// };
