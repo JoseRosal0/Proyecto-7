b05e111df66d412e7358ba3edb36f791d69470aa
Jose Rosal <andresrosalaponteee@gmail.com>
2024-09-04 13:23:49 -0400

    Vercion con comentarios

--- json.js
+++ json.js
@@ -1,3 +1,8 @@
+/*
+Arreglo de objetos de los cuales se optiene la informacion 
+a remplaza en el html si editas uno de los campos de los objetos y guardas
+se editara tambien en el html
+*/
 const hamburguers=[
     {
         name:"Burger Premium",
@@ -108,6 +113,68 @@ const tacos=[
         name:"Burger Premium",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
         img:"/images/hambuerger1.png",
+        price:15.00
+    }
+]
+const breakfast=[
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hamburger2.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hamburger2.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hamburger2.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hamburger2.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hamburger2.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hamburger2.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hamburger2.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hamburger2.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hamburger2.png",
+        price:15.00
+    },
+    {
+        name:"Burger Premium",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, saepe!",
+        img:"/images/hamburger2.png",
         price:15.00
     }
 ]
--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+/* esta variable es la cantidad de productos 
+desplegados antes de dar click al boton loadmore
+notaras que en efecto son 4
+*/
+var currentItem = 4; 
+/*
+DOMContentLoaded es un evento que se ejecuta cuando la pagina carga
+*/
 document.addEventListener("DOMContentLoaded",()=>{
-  console.log("holas dom")
-  createHtml(hamburguers,".box-container-1")
+    /*Llamamos a la funcion para crear el html de cada una de las ecciones 
+    con el arreglo de objetos que usara la funcion y con el id de cada seccion.
+    Los arreglos estan en el archivo json.js*/
+    createHtml(hamburguers,1)
+    createHtml(tacos,2)
+    createHtml(breakfast,3)
 })
-// console.log(hamburguers,"obj")
 
 function E(selector,parent){
     // console.log(selector,"hola selector");
@@ -29,12 +40,15 @@ function radioClass(element,clasName){
 }
 
 function tabs(nav) {
+    
     let navElem=E(nav)[0];
-    //console.log(navElem,"nav element")
     
     navElem.addEventListener("click",(e)=>{
+        currentItem=4
+        // console.log(currentItem,"current in tab")
+
         let target = e.target;
-        // console.log(target,"hola target")
+        // console.log(target.id,"hola target")
         //console.log(hasClass(target, "tab"),"Llamado de has")
 
 
@@ -45,6 +59,8 @@ function tabs(nav) {
         // console.log(linkedTab,"linked tab")
         radioClass(linkedTab, "visible");
 
+        
+
     });
 
     let active = E(".tab.active")[0];
@@ -52,124 +68,97 @@ function tabs(nav) {
         radioClass(E("."+active.id)[0],"visible");
     }
 }
-
-
-
-function createHtml(obj,selector){
-    console.log(obj,"hola objeto")
-    container =document.querySelector(selector)
+/*
+Funcion para crear el Html de manera dinamica,
+la funcion recive como parametro un arreglo (obj)
+y un id que representa una de las 3 secciones de comida
+Hamburguesas ,tacos o desayunos ;el id se concatena usando ${}
+para que se cree un selector dependiendo del id
+*/
+function createHtml(obj,id){
+    //container es el selector 
+    container =document.querySelector(`.box-container-${id}`)
+    /*iteramos en cada uno de los objetos dentro del arreglo
+    pasado como parametro usando el forEach
+    */
     obj.forEach(box=>{
-      const {name,description,img,price}=box;
-      let html=`
-      <div class="box-1">
-        <div class="image">
-            <img src="${img}" alt="">
-        </div>
+        /*box adquiere el valor de uno de los objetos 
+        en cada iteracion
+        */
+        const {name,description,img,price}=box;
+        /*asignamos variables que adquieren el valor de cada campo del objeto
+        esto es posible porque se llaman exactamente igual al campo
+        al cual quieren copiar el valor, esto se llama desturcturacion*/
 
-        <div class="content">
-
-            <h3>${name}</h3>
-            <p>${description}</p>
-            <div class="icons">
-                <span class="price">$${price}</span>
-                <span class="buy">
-                    <img src="/images/car.svg" alt="">
-                </span>
+        /*Creamos una varible con el codigo html que queremos crear 
+        ente backticks (``) ya que asi podemos colocar variables en el codigo
+        y colocamos las variables en su determinado puesto
+        */
+        let html=`
+        <div class="box-${id}">
+            <div class="image">
+                <img src="${img}" alt="">
             </div>
 
+            <div class="content">
+
+                <h3>${name}</h3>
+                <p>${description}</p>
+                <div class="icons">
+                    <span class="price">$${price}</span>
+                    <span class="buy">
+                        <img src="/images/car.svg" alt="">
+                    </span>
+                </div>
+
+            </div>
         </div>
-      </div>
-    `;
-
-    container.innerHTML+= html
+        `;
+        /*Concatenamos el resultado una y otra vez en el selector que 
+        hicimos al principio y asi se iran creando elementos en el html*/
+        container.innerHTML+= html;
     })
-
-      
-    
 }
 
 tabs(".menu-nav");
-
-// let loadMoreBtn1 = document.querySelector('#load-more-1');
-// let currentItem1 = 4;  // Starting index for displaying items
-
-// loadMoreBtn1.onclick = () => {
-//   createHtml()
-//   // console.log("ingreso a load more"); // Optional console log for debugging
-
-//   // Get all box elements within the container with class "box-container-1"
-//     let boxes = [...document.querySelectorAll(".box-container-1 .box-1")];
-//     console.log(boxes,"hola boxes")
-//   // Loop to display the next 4 items
-//     for (let i = currentItem1; i < currentItem1 + 4 && i < boxes.length; i++) {
-//         console.log("dentro del for");
-//         boxes[i].style.display = 'inline-block';
-//     }
-
-//   // Update currentItem for the next click
-//     currentItem1 += 4;
-
-//     // Hide the button if all items have been displayed
-//     if (currentItem1 >= boxes.length) {
-//         loadMoreBtn1.style.display = 'none';
-//     }
-// };
-
+/*Se crean los selectores de cada boton loadmore*/
 let loadMoreBtn1 = document.querySelector('.load-more-1');
 let loadMoreBtn2 = document.querySelector('.load-more-2');
 let loadMoreBtn3 = document.querySelector('.load-more-3');
- // Starting index for displaying items
 
+/*evento click en cada boton loadmore ,que llama a una funcion 
+generica que sirve para cada uno de los casos*/
 loadMoreBtn1.addEventListener("click",load)
 loadMoreBtn2.addEventListener("click",load)
 loadMoreBtn3.addEventListener("click",load)
-var currentItem = 4; 
 
-
+/*funcion que sirve para los 3 botones ya que el selector
+se crea mediante el id */
 function load(e){
-  
-  console.log("ingreso a load more"); // Optional console log for debugging
-  let id= parseInt(e.target.id);
-  console.log(e.target,"target")
-  // Get all box elements within the container with class "box-container-2"
+    /*con el parametro e (event) usamos e.target.id y asi obtenemos el id del 
+    elemnto html donde sucedio el evento
+    y con parseInt lo volvemos entero*/
+    let id= parseInt(e.target.id);
+    /*Creamos un arreglo que contenga todos los elementos con la clase
+    box mas el id ejemplo: box-1*/
     let boxes = [...document.querySelectorAll(`.box-${id}`)];
-  console.log(boxes)
-  // Loop to display the next 4 items
+    /*bucle que se ejecuta mientras i sea menor a currentItem +4 
+    y ademas mientras i sea menor a la cantidad de elementos dentro de 
+    nuetro arreglo boxes
+    */
     for (let i = currentItem; i < currentItem + 4 && i < boxes.length; i++) {
-        console.log("dentro del for",i);
-        
-        console.log(currentItem)
+        /*a cada elemento de boxes le editamos sus clases y le damos display 
+        inline-block lo que hace que de en 4 en 4 se muentren mas productos
+        luego de dar click en el boton load-more*/
         boxes[i].style.display = 'inline-block';
     }
 
   // Update currentItem for the next click
     currentItem+=4
+    
+
     // Hide the button if all items have been displayed
     if (currentItem >= boxes.length) {
         e.target.style.display = 'none';
     }
 };
-
-// let loadMoreBtn3 = document.querySelector('#load-more-3');
-// let currentItem3 = 4;  // Starting index for displaying items
-
-// loadMoreBtn3.onclick = () => {
-//   // console.log("ingreso a load more"); // Optional console log for debugging
-
-//   // Get all box elements within the container with class "box-container-3"
-//     let boxes = [...document.querySelectorAll(".box-container-3 .box-3")];
-
-//   // Loop to display the next 4 items
-//     for (let i = currentItem3; i < currentItem3 + 4 && i < boxes.length; i++) {
-//         console.log("dentro del for");
-//         boxes[i].style.display = 'inline-block';
-//     }
-
-//   // Update currentItem for the next click
-//     currentItem3 += 4;
-
-//     // Hide the button if all items have been displayed
-//     if (currentItem3 >= boxes.length) {
-//         loadMoreBtn3.style.display = 'none'; 
-//     }
-// };
